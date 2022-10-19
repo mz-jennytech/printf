e0781dbfc2938bd222c2590ee8fae8d06c934699
AbooJa01 <akeemsalaam@gmail.com>
2022-10-18 17:35:23 -0700

    Updated the main.h file

--- main.h
+++ main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_PRINTF_H
+#define MAIN_PRINTF_H
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -114,4 +114,4 @@ int is_digit(char);
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
-#endif /* MAIN_H */
+#endif /* MAIN_PRINTF_H */
